e34663db82f2402523723915468f5eb4c117a62f
William Villanueva <wvillanu89@gmail.com>
2014-07-08 15:17:52 -0700

    Produced working version.  Code needs refactoring, and the solution is naive.  Thanks to deibelman/dart-mic for the library

--- server.js
+++ server.js
@@ -8,17 +8,9 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.engine('html', ejs.renderFile);
 
-var filepath = path.join(__dirname, 'Paradise.m4a');
-
 app.get('/', function(req, res) {
 	res.render('index.html')
 });
 
-app.get('/music', function(req, res){
-	console.log('yoloooooooooooo');
-	res.set({'Content-Type': 'audio/mpeg'});
-	var readStream = fs.createReadStream(filepath);
-	readStream.pipe(res);
-})
 
 app.listen(8000);
